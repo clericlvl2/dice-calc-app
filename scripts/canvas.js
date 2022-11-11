c012b0d9c50154f531ad5e4f50856426707c1bcd
clericlvl2 <budarnykh.a@yandex.ru>
2022-11-11 17:15:58 +0700

    add basic drawing functionality to canvas

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,14 +1,56 @@
 function draw() {
   const canvas = document.querySelector('.board__canvas-window');
+  canvas.width = 300;
+  canvas.height = 250;
 
   if (canvas.getContext) {
     const ctx = canvas.getContext('2d');
+    const canvasBody = canvas.getBoundingClientRect();
+    const ctxParams = {
+      isDrawing: false,
+      previousX: null,
+      previousY: null,
+      currentX: null,
+      currentY: null,
+    }
+    function getCursorPosition(evt) {
+      const posX = evt.clientX - canvasBody.left;
+      const posY = evt.clientY - canvasBody.top;
+      return {x: posX, y: posY}
+    }
 
-    ctx.fillStyle = "rgb(200, 0, 0)";
-    ctx.fillRect(10, 10, 50, 50);
+    canvas.addEventListener('mousedown', () => {
+      ctxParams.isDrawing = true;
+    });
 
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect(30, 30, 50, 50);
+    canvas.addEventListener('mouseup', () => {
+      ctxParams.isDrawing = false;
+      ctxParams.currentX = null;
+      ctxParams.currentY = null;
+      ctxParams.previousX = null;
+      ctxParams.previousY = null;
+    });
+
+    canvas.addEventListener('mousemove', evt => {
+      if (ctxParams.isDrawing) {
+        const position = getCursorPosition(evt);
+
+        if (!ctxParams.previousX || !ctxParams.previousY) {
+          ctxParams.previousX = position.x;
+          ctxParams.previousY = position.y;
+        }
+        ctxParams.currentX = position.x;
+        ctxParams.currentY = position.y;
+
+        ctx.beginPath();
+        ctx.moveTo(ctxParams.previousX, ctxParams.previousY);
+        ctx.lineTo(ctxParams.currentX, ctxParams.currentY);
+        ctx.stroke();
+
+        ctxParams.previousX = ctxParams.currentX;
+        ctxParams.previousY = ctxParams.currentY
+      }
+    });
   } else {
     // canvas-unsupported code here
   }
